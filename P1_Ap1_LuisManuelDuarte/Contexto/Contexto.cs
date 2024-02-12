25e7488bb66d6e1592ba0f552a1c9d89375dba11
Luis Manuel Duarte Bobadilla <luis_duarte@ucne.edu.do>
2024-02-12 16:31:18 -0600

    Creacion del Contexto

---
+++ P1_Ap1_LuisManuelDuarte/Contexto/Contexto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P1_Ap1_LuisManuelDuarte.Models;
+namespace P1_Ap1_LuisManuelDuarte.Contexto
+{
+    public class Contexto : DbContext
+    {
+
+        public Contexto(DbContextOptions<Contexto> Options) : base(Options)
+        {
+        }
+
+        public DbSet<Metas> Metas { get; set; }
+
+    }
+}
+    

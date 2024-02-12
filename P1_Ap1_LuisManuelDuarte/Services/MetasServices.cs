40a593df0d1d00b15f2aef964b234961546ea56e
Luis Manuel Duarte Bobadilla <luis_duarte@ucne.edu.do>
2024-02-12 17:05:02 -0600

    Cree el Services

--- P1_Ap1_LuisManuelDuarte/Services/MetasServices.cs
+++ P1_Ap1_LuisManuelDuarte/Services/MetasServices.cs
@@ -1,8 +1,80 @@
-﻿namespace P1_Ap1_LuisManuelDuarte.Services
+﻿
+using Microsoft.EntityFrameworkCore;
+using P1_Ap1_LuisManuelDuarte.DAL;
+using P1_Ap1_LuisManuelDuarte.Models;
+using System.Linq.Expressions;
+
+
+namespace P1_Ap1_LuisManuelDuarte.Services
 {
     public class MetasServices
     {
+        private readonly Contexto _contexto;
 
+        public MetasServices(Contexto contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public async Task<bool> Existe(int metasId)
+        {
+            return await _contexto.Metas.AnyAsync(c => c.MetaId == metasId);
+        }
+
+        public async Task<bool> Insertar(Metas metas)
+        {
+            _contexto.Add(metas);
+            return await _contexto.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> Modificar(Metas metas)
+        {
+            _contexto.Update(metas);
+            return await _contexto.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> Guardar(Metas metas)
+        {
+            if (!await Existe(metas.MetaId))
+            {
+                return await Insertar(metas);
+            }
+            else
+            {
+                return await Modificar(metas);
+            }
+        }
+
+        public async Task<bool> Eliminar(Metas metas)
+        {
+            var cantidad = await _contexto.Metas
+                .Where(c => c.MetaId == metas.MetaId)
+                .ExecuteDeleteAsync();
+
+            return cantidad > 0;
+        }
+
+        public async Task<Metas?> Buscar(int metaId)
+        {
+            return await _contexto.Metas
+                .AsNoTracking()
+                .FirstOrDefaultAsync(c => c.MetaId == metaId);
+        }
+
+        public async Task<List<Metas>> GetList(Expression<Func<Metas, bool>> criterio)
+        {
+            return await _contexto.Metas
+                .Where(criterio)
+                .AsNoTracking()
+                .ToListAsync();
+        }
+
+        public async Task<Metas?> BuscarId(int metasId)
+        {
+            return await _contexto.Metas
+                .AsNoTracking()
+                .FirstOrDefaultAsync(c => c.MetaId == metasId);
+        }
 
 
     }

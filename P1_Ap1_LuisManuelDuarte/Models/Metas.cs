5b9ad329139974dd6c479ff56ed3239c49ba1707
Luis Manuel Duarte Bobadilla <luis_duarte@ucne.edu.do>
2024-02-12 16:23:06 -0600

    Creacion del Modelo

---
+++ P1_Ap1_LuisManuelDuarte/Models/Metas.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P1_Ap1_LuisManuelDuarte.Models
+{
+    public class Metas
+    {
+        [Key]
+        public int MetaId { get; set; }
+
+        
+        [DataType(DataType.Date)]
+        public DateTime Fecha {  get; set; }= DateTime.Now;
+
+        [Required(ErrorMessage = "La Descripcion es Requerida")]
+        public string Descripcion { get; set; }
+
+        [Required(ErrorMessage = "")]
+        public float Monto { get; set; }
+
+    }
+}
